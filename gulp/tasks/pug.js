f89e91eedb51341a8e9e25b6d076da83ca6e2b8d
Egor Dyachenko <whitebestnik@gmail.com>
2018-06-02 16:40:59 +0300

    Внедрил сборку

---
+++ gulp/tasks/pug.js
@@ -0,0 +1,36 @@
+module.exports = function () {
+  const p = global.p;
+  p.gulp.task("pug", function () {
+    return p.gulp
+      .src(p.paths.src.pug)
+      .pipe(p.gp.newer(p.paths.build.html)) // Проверяем из кеша
+      .pipe(
+        p.gp.plumber({
+          errorHandler: p.gp.notify.onError(function (error) {
+            return {
+              title: "PUG - ошибка при сборке .pug",
+              message: error.message,
+            };
+          }),
+        })
+      ) // Предотвращает остановку плагина при возникновении ошибки
+      .pipe(
+        p.gp.pug({
+          pretty: true,
+        })
+      ) // Конвертируем
+      .pipe(p.gulp.dest(p.paths.build.html)) // Копируем
+      .pipe(
+        p.gp.htmlhint({
+          "attr-lowercase": false,
+          "spec-char-escape": false,
+        })
+      ) // Валидация
+      .pipe(p.gp.htmlhint.failAfterError())
+      .pipe(
+        p.browserSync.reload({
+          stream: true,
+        })
+      );
+  });
+};
